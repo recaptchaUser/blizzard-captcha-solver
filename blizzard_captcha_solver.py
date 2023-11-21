0cf53cbf9764dcee16d31ac605066f9e6213b7dc
recaptchaUser <151567702+recaptchaUser@users.noreply.github.com>
2023-11-21 18:48:24 +0100

    Create blizzard_captcha_solver.py

---
+++ blizzard_captcha_solver.py
@@ -0,0 +1,27 @@
+import capsolver
+from urllib.parse import urlparse
+
+# Change this information
+capsolver.api_key = "Your pay per usage key"
+
+def solve_funcaptcha_blizzard():
+    solution = capsolver.solve({
+   "type": "FunCaptchaTaskProxyLess",
+        "websiteURL": "https://eu.account.battle.net",
+        "websitePublicKey": "CF5C61EE-F062-45DF-A32F-E9398E2B4E0D",
+        "funcaptchaApiJSSubdomain": "https://blizzard-api.arkoselabs.com"
+        "
+    })
+    return solution
+
+def main():
+    
+    print("Solving blizzard captcha")
+    solution = solve_funcaptcha_blizzard()
+    
+    token = solution["token"]
+    
+    print("Token Solution " + token)
+    
+if __name__ == "__main__":
+    main()
